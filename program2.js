5b91142fc56853b68658ac49a9829726551c4207
harsgsirt <tbtharshgupta@gmail.com>
2024-09-27 14:51:36 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,31 +1,34 @@
-class Solution {
-    romanToInt(s) {
-        const romanMap = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        };
-        
-        let total = 0;
-        let prevValue = 0;
+function romanToInt(s) {
+    const romanMap = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
 
-        for (let i = s.length - 1; i >= 0; i--) {
-            const currentValue = romanMap[s[i]];
-            if (currentValue < prevValue) {
-                total -= currentValue;
-            } else {
-                total += currentValue; 
-            }
-            prevValue = currentValue; 
+    let total = 0;
+    let prevValue = 0;
+
+    for (let char of s) {
+        const currentValue = romanMap[char];
+        total += currentValue;
+
+        // If the previous value is less than the current value,
+        // subtract double the previous value (it was added once already)
+        if (prevValue < currentValue) {
+            total -= 2 * prevValue;
         }
 
-        return total;
+        prevValue = currentValue;
     }
+
+    return total;
 }
+
+module.exports = { romanToInt }; // Ensure this line is included to export the function
 
 
 
